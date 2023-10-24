4c25654d155b939af7d10499fe6d3cfb8dfb9e58
AdityaHadesta <adityahadesta@gmail.com>
2023-10-24 14:49:35 +0700

    CustomWidget_2034

---
+++ part9/lib/mycontainer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Mycontainer extends StatelessWidget {
+  const Mycontainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('HAI'),
+    );
+  }
+}
+
+//
